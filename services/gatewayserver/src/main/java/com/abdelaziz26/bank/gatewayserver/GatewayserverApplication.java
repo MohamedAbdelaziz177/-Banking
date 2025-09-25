5877fd3cd2dc99e5bf855f63261c8099b2ad2099
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-09-25 09:25:21 +0300

    initializing the basic services

---
+++ services/gatewayserver/src/main/java/com/abdelaziz26/bank/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,13 @@
+package com.abdelaziz26.bank.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+}
---
+++ services/gatewayserver/src/test/java/com/abdelaziz26/bank/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdelaziz26.bank.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
