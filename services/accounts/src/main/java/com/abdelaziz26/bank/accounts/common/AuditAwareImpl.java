f1d7bb49d085419121ff10889e39f58558a74df6
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-10-03 11:29:54 +0300

    adding jpa auditing to account entity

---
+++ services/accounts/src/main/java/com/abdelaziz26/bank/accounts/common/AuditAwareImpl.java
@@ -0,0 +1,14 @@
+package com.abdelaziz26.bank.accounts.common;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("AuditorAware")
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("accounts-ms");
+    }
+}
