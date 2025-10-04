9121076ca04bbc745604bf8db5df681a612ca10e
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-10-04 07:59:58 +0300

    Api gateway added, configured, registered with eureka

--- services/gatewayserver/src/main/java/com/abdelaziz26/bank/gatewayserver/GatewayserverApplication.java
+++ services/gatewayserver/src/main/java/com/abdelaziz26/bank/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,11 @@ package com.abdelaziz26.bank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +15,28 @@ public class GatewayserverApplication {
         SpringApplication.run(GatewayserverApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r ->
+                        r.path("/accounts/**")
+                                .filters(f -> f
+                                        .addRequestHeader("time-stamp", LocalDateTime.now().toString())
+                                        .addResponseHeader("service", "ACC_SERVICE"))
+                                .uri("lb://ACCOUNTS"))
+                .route(r ->
+                        r.path("/cards/**")
+                                .filters(f -> f
+                                        .addRequestHeader("time-stamp", LocalDateTime.now().toString())
+                                        .addResponseHeader("service", "CARD_SERVICE"))
+                                .uri("lb://CARDS"))
+                .route(r ->
+                        r.path("/loans/**")
+                                .filters(f -> f
+                                        .addRequestHeader("time-stamp", LocalDateTime.now().toString())
+                                        .addResponseHeader("service", "LOAN_SERVICE")
+                                )
+                                .uri("lb://LOANS"))
+                .build();
+    }
 }
